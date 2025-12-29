bb7dac043fa15bc9466446168372e93906a566aa
Gris Ge <fge@redhat.com>
2025-12-29 17:13:25 +0800

    conf: Support changing bond port configurations
    Example YAML:
    
    ```
    ---
    interfaces:
      - name: bond99
        type: bond
        bond:
          mode: active-backup
      - name: dummy1
        type: dummy
        controller: bond99
        bond-port:
          priority: -10
          queue-id: 1
      - name: dummy2
        type: dummy
        state: up
        controller: bond99
        bond-port:
          priority: -20
          queue-id: 2
    ```
    
    Integration test case updated.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/lib/conf/iface.rs
+++ src/lib/conf/iface.rs
@@ -8,12 +8,13 @@ use rtnetlink::{
 use serde::{Deserialize, Serialize};
 
 use super::{
-    alt_name::change_iface_alt_name, base_iface::apply_base_link_changes,
-    ip::change_ip_layer,
+    super::query::get_ifaces_with_handle, alt_name::change_iface_alt_name,
+    base_iface::apply_base_link_changes, ip::change_ip_layer,
 };
 use crate::{
-    AltNameConf, BondConf, BridgeConf, DummyConf, ErrorKind, Iface, IfaceState,
-    IfaceType, IpConf, NisporError, VethConf, VlanConf,
+    AltNameConf, BondConf, BondPortConf, BridgeConf, DummyConf, ErrorKind,
+    Iface, IfaceState, IfaceType, IpConf, NetStateIfaceFilter, NisporError,
+    VethConf, VlanConf,
 };
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Default)]
@@ -38,6 +39,7 @@ pub struct IfaceConf {
     pub vlan: Option<VlanConf>,
     #[serde(alias = "link-aggregation")]
     pub bond: Option<BondConf>,
+    pub bond_port: Option<BondPortConf>,
 }
 
 impl IfaceConf {
@@ -75,8 +77,12 @@ fn default_iface_state_in_conf() -> IfaceState {
 pub(crate) async fn apply_iface_conf(
     handle: &rtnetlink::Handle,
     des_iface: &IfaceConf,
-    cur_iface: Option<&Iface>,
 ) -> Result<(), NisporError> {
+    // Instead of query full current state, we query desired interface only
+    // one by one, because:
+    // 1. Consistent performance regardless current interface amount.
+    // 2. Allowing removing interface and add it back sequentially.
+    let cur_iface = get_cur_iface(handle, des_iface).await;
     if des_iface.state == IfaceState::Absent {
         if let Some(cur_iface) = cur_iface {
             delete_iface(handle, &des_iface.name, cur_iface.index).await?;
@@ -87,7 +93,8 @@ pub(crate) async fn apply_iface_conf(
             );
         }
     } else {
-        let mut msgs = gen_link_msg(handle, des_iface, cur_iface).await?;
+        let mut msgs =
+            gen_link_msg(handle, des_iface, cur_iface.as_ref()).await?;
         if cur_iface.is_some() {
             for msg in msgs {
                 send_change_netlink(handle, msg, des_iface.name.as_str())
@@ -116,8 +123,13 @@ pub(crate) async fn apply_iface_conf(
             }
         }
 
-        change_iface_alt_name(handle, des_iface, cur_iface).await?;
-        change_ip_layer(handle, des_iface).await?;
+        // Refresh current interface for newly created interface or
+        // controller/port attached
+        if let Some(cur_iface) = get_cur_iface(handle, des_iface).await {
+            change_port_config(handle, des_iface, &cur_iface).await?;
+            change_iface_alt_name(handle, des_iface, &cur_iface).await?;
+            change_ip_layer(handle, des_iface, &cur_iface).await?;
+        }
     }
 
     Ok(())
@@ -168,13 +180,23 @@ async fn gen_link_msg(
             .await?
         }
         Some(IfaceType::Dummy) => {
-            apply_base_link_changes(
-                handle,
-                DummyConf::gen_link_msg_builder(des_iface),
-                des_iface,
-                cur_iface,
-            )
-            .await?
+            if cur_iface.is_none() {
+                apply_base_link_changes(
+                    handle,
+                    DummyConf::gen_link_msg_builder(des_iface),
+                    des_iface,
+                    cur_iface,
+                )
+                .await?
+            } else {
+                apply_base_link_changes(
+                    handle,
+                    LinkUnspec::new_with_name(des_iface.name.as_str()),
+                    des_iface,
+                    cur_iface,
+                )
+                .await?
+            }
         }
         _ => {
             apply_base_link_changes(
@@ -225,4 +247,38 @@ async fn send_change_netlink(
                 format!("Failed to change interface {iface_name}: {e}"),
             )
         })
+}
+
+async fn get_cur_iface(
+    handle: &rtnetlink::Handle,
+    des_iface: &IfaceConf,
+) -> Option<Iface> {
+    let mut iface_filter = NetStateIfaceFilter::minimum();
+    iface_filter.iface_name = Some(des_iface.name.to_string());
+    if des_iface.ipv4.is_some() || des_iface.ipv6.is_some() {
+        iface_filter.include_ip_address = true;
+    }
+    if let Ok(mut cur_ifaces) =
+        get_ifaces_with_handle(&handle, Some(&iface_filter)).await
+    {
+        cur_ifaces.remove(&des_iface.name)
+    } else {
+        None
+    }
+}
+
+async fn change_port_config(
+    handle: &rtnetlink::Handle,
+    des_iface: &IfaceConf,
+    cur_iface: &Iface,
+) -> Result<(), NisporError> {
+    if let Some(bond_port_conf) = des_iface.bond_port.as_ref() {
+        send_change_netlink(
+            handle,
+            bond_port_conf.gen_link_msg(cur_iface),
+            des_iface.name.as_str(),
+        )
+        .await?;
+    }
+    Ok(())
 }
